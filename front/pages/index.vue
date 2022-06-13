f29cde1ff832a33cfbc66026c26b6fd14810df51
works4729 <r.works4729@gmail.com>
2022-06-13 04:31:01 +0000

    Add front

---
+++ front/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button
+      type="button"
+      name="button"
+      @click="getMsg"
+    >
+      RailsからAPIを取得する
+    </button>
+    <div
+      v-for="(msg, i) in msgs"
+      :key="i"
+    >
+      {{ msg }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      subTitle: 'RailsからAPIを取得する',
+      msgs: []
+    }
+  },
+  methods: {
+    getMsg () {
+      this.$axios.$get('/hello')
+        .then(res => this.msgs.push(res))
+    }
+  }
+}
+</script>
